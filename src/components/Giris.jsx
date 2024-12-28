7f7394a79f78f9ed3bb3808fdbfd1e0fb8e15870
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2024-12-29 02:56:13 +0300

    Update Giris.jsx

--- src/components/Giris.jsx
+++ src/components/Giris.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../css/Giris.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 import { faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons"; // Doğru seti kullanın
 import girisimage from "../img/giris.png";
 
@@ -9,65 +9,69 @@ function Giris() {
   return (
     <div className="site-wrapper">
       <div className="content">
-        <h2>İlayda</h2>
-        <div className="navbar-giris">
-          <h3 style={{ color: "#D9D9D9" }}>
-            <span style={{ color: "#CAF181" }}>TÜRKÇE</span>’YE GEÇ
-          </h3>
-          <label class="switch">
-            <input type="checkbox" checked />
-            <span class="slider round"></span>
-          </label>
-          <h3 style={{ color: "#4731D3" }}>DARK MODE</h3>
-        </div>
-        <div className="image-giris">
-          <img src={girisimage}></img>
-        </div>
-        <div>
-          <h1>
-            I am a Frontend <br /> Developer...{" "}
-          </h1>
+        <header className="header">
+          <h2>İlayda</h2>
+          <div className="navbar-giris">
+            <h4 style={{ color: "#D9D9D9" }}>
+              <span style={{ color: "#CAF181" }}>TÜRKÇE</span>’YE GEÇ
+            </h4>
+            <label class="switch">
+              <input type="checkbox" checked />
+              <span class="slider round"></span>
+            </label>
+            <h4 style={{ color: "#c8c5da" }}>DARK MODE</h4>
+          </div>
+        </header>
+        <div className="hero-content">
+          <div className="hero-left">
+            <h1>
+              I am a Frontend <br /> Developer...{" "}
+            </h1>
 
-          <p>
-            ...who likes to craft solid and scalable frontend <br /> products
-            with great user experiences.
-          </p>
-          <button
-            style={{
-              color: "#3730A3",
-              fontSize: "18px",
-              border: "1px solid #3730A3",
-              lineHeight: "28px",
-              fontWeight: "500px",
-            }}
-          >
-            <FontAwesomeIcon
-              icon={faGithub}
+            <p className="giris-p">
+              ...who likes to craft solid and scalable frontend <br /> products
+              with great user experiences.
+            </p>
+            <button
               style={{
-                width: "26px",
-                height: "26.26px",
+                color: "#3730A3",
+                fontSize: "18px",
+                border: "1px solid #3730A3",
+                lineHeight: "28px",
+                fontWeight: "500px",
               }}
-            />
-            Github
-          </button>
-          <button
-            style={{
-              color: "#3730A3",
-              fontSize: "18px",
-              lineHeight: "28px",
-              fontWeight: "500px",
-              border: "1px solid #3730A3",
-            }}
-          >
-            <FontAwesomeIcon
-              icon={faLinkedinIn}
+            >
+              <FontAwesomeIcon
+                icon={faGithub}
+                style={{
+                  width: "26px",
+                  height: "26.26px",
+                }}
+              />
+              Github
+            </button>
+            <button
               style={{
-                width: "24px",
-                height: "25.21px",
+                color: "#3730A3",
+                fontSize: "18px",
+                lineHeight: "28px",
+                fontWeight: "500px",
+                border: "1px solid #3730A3",
               }}
-            />
-            Linkedin
-          </button>
+            >
+              <FontAwesomeIcon
+                icon={faLinkedinIn}
+                style={{
+                  width: "24px",
+                  height: "25.21px",
+                }}
+              />
+              Linkedin
+            </button>
+          </div>
+          <div className="image-giris">
+            <img src={girisimage}></img>
+          </div>
         </div>
       </div>
     </div>
