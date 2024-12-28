3cbdfe0ca5a8eed9106075ec3c9b636f182933af
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2024-12-29 02:56:10 +0300

    Update Projects.jsx

---
+++ src/components/Projects.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./Card";
+import "../css/Projects.css";
+function Projects() {
+  return (
+    <div className="projects-bg">
+      <h1 style={{ color: "#4731D3" }}>Projects</h1>
+      <Card />
+    </div>
+  );
+}
+
+export default Projects;
