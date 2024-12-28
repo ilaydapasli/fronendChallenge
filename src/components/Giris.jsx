8cdc4659c07c1d16c717b089037a9f62f889bdd0
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2024-12-28 16:36:20 +0300

    Update Giris.jsx

--- src/components/Giris.jsx
+++ src/components/Giris.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "../css/Giris.css";
-import "font-awesome/css/font-awesome.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons"; // Doğru seti kullanın
+import girisimage from "../img/giris.png";
 
 function Giris() {
   return (
@@ -8,14 +11,18 @@ function Giris() {
       <div className="content">
         <h2>İlayda</h2>
         <div className="navbar-giris">
-          <h3>TÜRKÇE’YE GEÇ</h3>
+          <h3 style={{ color: "#D9D9D9" }}>
+            <span style={{ color: "#CAF181" }}>TÜRKÇE</span>’YE GEÇ
+          </h3>
           <label class="switch">
             <input type="checkbox" checked />
             <span class="slider round"></span>
           </label>
-          <h3>DARK MODE</h3>
+          <h3 style={{ color: "#4731D3" }}>DARK MODE</h3>
         </div>
-
+        <div className="image-giris">
+          <img src={girisimage}></img>
+        </div>
         <div>
           <h1>
             I am a Frontend <br /> Developer...{" "}
@@ -25,8 +32,42 @@ function Giris() {
             ...who likes to craft solid and scalable frontend <br /> products
             with great user experiences.
           </p>
-          <button>Github</button>
-          <button>Linkedin</button>
+          <button
+            style={{
+              color: "#3730A3",
+              fontSize: "18px",
+              border: "1px solid #3730A3",
+              lineHeight: "28px",
+              fontWeight: "500px",
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              style={{
+                width: "26px",
+                height: "26.26px",
+              }}
+            />
+            Github
+          </button>
+          <button
+            style={{
+              color: "#3730A3",
+              fontSize: "18px",
+              lineHeight: "28px",
+              fontWeight: "500px",
+              border: "1px solid #3730A3",
+            }}
+          >
+            <FontAwesomeIcon
+              icon={faLinkedinIn}
+              style={{
+                width: "24px",
+                height: "25.21px",
+              }}
+            />
+            Linkedin
+          </button>
         </div>
       </div>
     </div>
