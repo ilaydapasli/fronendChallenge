1ad7bf01fe0095c36a184a05e0a6460e1b1eae76
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2024-12-30 18:51:03 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+
 import Giris from "./components/Giris";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 import Skills from "./components/Skills";
 import Profile from "./components/Profile";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+import Bos from "./components/Bos";
 function App() {
   return (
     <>
       <div className="container">
         <Giris userName="ilayda" />
         <Skills />
+        <Profile />
         <Projects />
-        <Projects />
-        <Footer />{" "}
+        <Footer />
+        <Bos />
       </div>
     </>
   );
