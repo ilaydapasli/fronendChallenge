a99f626cce08f10aa062eee8d577ac2746cd388b
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2024-12-30 18:50:59 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,18 +6,33 @@ import {
   faWhatsapp,
   faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
-import "../css/Footer.css";
+
 function Footer() {
   return (
-    <div className="footer-container">
-      <div className="footer-content">
-        <h1 style={{ color: "#4731D3" }}>Send me a message!</h1>
-        <p>Got a question or proposal, or just want to say hello? Go ahead.</p>
-        <a href="">almilasucode@gmail.com</a>
+    <div className="footer-container  max-h-screen flex flex-col text-center mt-20 px-4 mb-20">
+      <div className="footer-content flex flex-col p-4 ">
+        <h1 className="p-8 text-[48px]" style={{ color: "#4731D3" }}>
+          Send me a message!
+        </h1>
+        <p className="p-8 text-[24px] leading-9	">
+          Got a question or proposal, or just want <br /> to say hello? Go
+          ahead.
+        </p>
+        <a className="text-[#4731D3] leading-9	underline  text-[20px] " href="">
+          almilasucode@gmail.com
+        </a>
 
-        <div>
-          <FontAwesomeIcon icon={faTwitter} style={{ color: "#4731D3" }} />
-          <FontAwesomeIcon icon={faCodepen} style={{ color: "#4731D3" }} />
+        <div className=" flex gap-2 justify-center mt-3">
+          <FontAwesomeIcon
+            className=""
+            icon={faTwitter}
+            style={{ color: "#4731D3" }}
+          />
+          <FontAwesomeIcon
+            className=""
+            icon={faCodepen}
+            style={{ color: "#4731D3" }}
+          />
 
           <FontAwesomeIcon icon={faWhatsapp} style={{ color: "#4731D3" }} />
           <FontAwesomeIcon icon={faInstagram} style={{ color: "#4731D3" }} />
