38dfdd946a904aab162a2bc25a21c7648e77a497
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2024-12-30 03:57:01 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        screens: {},
+      },
+      colors: {
+        primary: "#4731D3", // Örnek bir mor
+        secondary: "#CBF281", // Örnek bir yeşil
+        darkprimary: "#171043", // Örnek bir yeşil tonu
+        darksecondary: "#1A210B", // Örnek bir gri tonu
+        darkgrey: "##2B2727",
+
+        buttonColor: "#3730A3",
+      },
+    },
   },
   plugins: [],
-}
-
+};
