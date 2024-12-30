e433535e2e927c8d1c9992281b4ae49ce5d085f7
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2024-12-30 03:57:34 +0300

    Create data.js

---
+++ src/api/data.js
@@ -0,0 +1,43 @@
+import javascript from "../img/skillsicons/js.png";
+import react from "../img/skillsicons/react.png";
+import redux from "../img/skillsicons/redux.png";
+import node from "../img/skillsicons/node.png";
+import vsCode from "../img/skillsicons/vsCode.png";
+import figma from "../img/skillsicons/figma.png";
+
+export const girisData = {
+  title: {
+    en: "I am a Frontend Developer...",
+    tr: "Ben Frontend Developerım",
+  },
+  description: {
+    en: "...who likes to craft solid and scalable frontend products with great user experiences.",
+    tr: "Harika kullanıcı deneyimlerine sahip sağlam ve ölçeklenebilir ön uç ürünleri oluşturmayı seven biriyim.",
+  },
+  skills: [
+    {
+      name: "JavaScript",
+      image: javascript,
+    },
+    {
+      name: "React",
+      image: react,
+    },
+    {
+      name: "Redux",
+      image: redux,
+    },
+    {
+      name: "Node.js",
+      image: node,
+    },
+    {
+      name: "VS Code",
+      image: vsCode,
+    },
+    {
+      name: "Figma",
+      image: figma,
+    },
+  ],
+};
