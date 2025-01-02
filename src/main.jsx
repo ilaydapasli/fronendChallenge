49326456e5df511ff64c2a5671594b8595711b2f
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2025-01-02 16:52:42 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { SiteGlobalProvider } from "./context/SiteGlobalProvider.jsx";
+import { Provider } from "react-redux";
+import { myStore } from "./store/store.js";
 
-createRoot(document.getElementById("root")).render(<App />);
+// createRoot kullanarak render işlemi yapılır
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <SiteGlobalProvider>
+    <Provider store={myStore}>
+      <App />
+    </Provider>
+  </SiteGlobalProvider>
+);
