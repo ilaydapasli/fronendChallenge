4b4b3ec219343dcab8e2b2e16a5de6d4b060c490
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2025-01-02 16:53:19 +0300

    Update data.js

--- src/api/data.js
+++ src/api/data.js
@@ -5,7 +5,15 @@ import node from "../img/skillsimg/node.jpeg";
 import vsCode from "../img/skillsimg/vscode.png";
 import figma from "../img/skillsimg/figma.jpeg";
 
-export const girisData = {
+import twitter from "../img/socialmedia/twitter.svg";
+import codepen from "../img/socialmedia/codepen.svg";
+import atsign from "../img/socialmedia/at-sign.svg";
+import instagram from "../img/socialmedia/instagram.svg";
+import twitter_dark from "../img/socialmedia/twitter-dark.svg";
+import codepen_dark from "../img/socialmedia/codepen-dark.svg";
+import atsign_dark from "../img/socialmedia/at-sign-dark.svg";
+import instagram_dark from "../img/socialmedia/instagram-dark.svg";
+export const headerData = {
   title: {
     en: "I am a Frontend Developer...",
     tr: "Ben Frontend Developerım",
@@ -14,30 +22,45 @@ export const girisData = {
     en: "...who likes to craft solid and scalable frontend products with great user experiences.",
     tr: "Harika kullanıcı deneyimlerine sahip sağlam ve ölçeklenebilir ön uç ürünleri oluşturmayı seven biriyim.",
   },
-  skills: [
-    {
-      name: "JavaScript",
-      image: javascript,
-    },
-    {
-      name: "React",
-      image: react,
-    },
-    {
-      name: "Redux",
-      image: redux,
-    },
-    {
-      name: "Node.js",
-      image: node,
-    },
-    {
-      name: "VS Code",
-      image: vsCode,
-    },
-    {
-      name: "Figma",
-      image: figma,
-    },
-  ],
+};
+
+export const skillsData = [
+  {
+    image: javascript,
+    text: "JAVASCRIPT",
+  },
+  {
+    image: react,
+    text: "REACT",
+  },
+  {
+    image: redux,
+    text: "REDUX",
+  },
+  {
+    image: node,
+    text: "NODE",
+  },
+  {
+    image: vsCode,
+    text: "VSCODE",
+  },
+  {
+    image: figma,
+    text: "FIGMA",
+  },
+];
+
+export const skillsHeader = {
+  en: "Skills",
+  tr: "Yetenekler",
+};
+export const footerData = {
+  sendme: { en: "Send me a message!", tr: "Bana Ulaşın!" },
+  description: {
+    en: "Got a question or proposal, or just want to say hello? Go ahead.",
+    tr: "Bir sorunuz veya öneriniz mi var, yoksa sadece merhaba mı demek istiyorsunuz? İlerleyin.",
+  },
+  social: [twitter, codepen, atsign, instagram],
+  social_dark: [twitter_dark, codepen_dark, atsign_dark, instagram_dark],
 };
