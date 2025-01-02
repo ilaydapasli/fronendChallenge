25d7401d56dbd6d1998847199cf9b05d0ff2038b
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2025-01-02 22:32:39 +0300

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,53 +1,74 @@
 import React from "react";
-import "../css/Profile.css";
-import profimg from "../img/Profile.png";
-import Card from "./Card";
+//import "../css/Profile.css";
+import { SiteGlobalContext } from "../context/SiteGlobalProvider";
+import { useContext } from "react";
+import { profileData } from "../api/data";
+import ProfileBg from "../img/backgrounds/profile-bg.png";
+
+import darkProfileBg from "../img/backgrounds/profile-bg-dark.png";
 function Profile() {
+  const { theme, toggleDarkMode, lang, setLang } =
+    useContext(SiteGlobalContext);
+
   return (
     <div
-      className="profile-bg"
-      style={{ backgroundColor: "#4731D3", width: "100%" }}
+      className={`h-screen bg-cover bg-center flex flex-col `}
+      style={{
+        backgroundImage: `url(${theme === "dark" ? darkProfileBg : ProfileBg})`, // Tema seçimine göre arka planı değiştiriyoruz
+      }}
     >
-      <h1>Profile</h1>
-      <div className="profile-content">
-        <div className="basic-info">
-          <h3 style={{ color: "#fff" }}>Basic ınformation</h3>
-          <p>
-            <strong style={{ color: "#CBF281" }}>Doğum tarihi:</strong>{" "}
-            24.03.1996
-          </p>
-          <p>
-            <strong>İkamet Şehri:</strong> Ankara
-          </p>
-          <p>
-            <strong>Eğitim Durumu:</strong> Hacettepe Üniv. Biyoloji Lisans,
-            2016
-          </p>
-          <p>
-            <strong>Tercih Ettiği Rol:</strong> Frontend, UI
-          </p>
-          <div className="profile-info"></div>
+      <h2>{lang === "tr" ? profileData.header.tr : profileData.header.en}</h2>
+      <div className="profile-main-area">
+        <div className="profile-info-area">
+          <h4>
+            {lang === "tr"
+              ? profileData.firstHeader.tr
+              : profileData.firstHeader.en}
+          </h4>
+          <div className="profile-info">
+            <h6>
+              {lang === "tr"
+                ? profileData.personal.birthdate.tr
+                : profileData.personal.birthdate.en}
+            </h6>
+            <p>02.06.1999</p>
+          </div>
+          <div className="profile-info">
+            <h6>
+              {lang === "tr"
+                ? profileData.personal.location.tr
+                : profileData.personal.location.en}
+            </h6>
+            <p>Rize</p>
+          </div>
+          <div className="profile-info">
+            <h6>
+              {lang === "tr"
+                ? profileData.personal.education.tr
+                : profileData.personal.education.en}
+            </h6>
+            <p>Sivas Cumhuriyet üniversitesi,2020</p>
+          </div>
+          <div className="profile-info">
+            <h6>
+              {lang === "tr"
+                ? profileData.personal.role.tr
+                : profileData.personal.role.en}
+            </h6>
+            <p>Fullstack</p>
+          </div>
         </div>
-        <div>
-          {" "}
-          <img
-            style={{ width: "300px", height: "290.68px", borderRadius: "10px" }}
-            src={profimg}
-            alt=""
-          />
+        <div className="middle-area">
+          <img src={profileData.image} />
         </div>
-        <div className="about-box">
-          <h3>About Me</h3>
-          <p>
-            {" "}
-            Lorem ipsum, dolor sit amet <br />
-            consectetur adipisicing elit. <br /> Veniam aut, odit laborum
-            aliquam <br /> voluptatum nisi mollitia.
-            <br />
-            Mnima accusamus ratione soluta <br /> aperiam sit voluptate? Dicta
-            quod <br /> deserunt quam temporibus <br />
-            cumque magnam!{" "}
-          </p>
+        <div className="about-area">
+          <h4>
+            {lang === "tr"
+              ? profileData.lastHeader.tr
+              : profileData.lastHeader.en}
+          </h4>
+          <p>{profileData.description1}</p>
+          <p>{profileData.description2}</p>
         </div>
       </div>
     </div>
