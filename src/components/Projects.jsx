02fb2b58471f5d9810b869cfffa6ab2f1cf41987
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2025-01-02 22:32:37 +0300

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,47 @@
 import React from "react";
-import Card from "./Card";
-import "../css/Projects.css";
+import { SiteGlobalContext } from "../context/SiteGlobalProvider";
+
+//import "../css/Projects.css";
+import { projectData } from "../api/data";
+import { projectsHeader } from "../api/data";
+import { useContext } from "react";
 function Projects() {
+  const { lang } = useContext(SiteGlobalContext);
+
   return (
     <div className="projects-bg">
-      <h1 style={{ color: "#4731D3" }}>Projects</h1>
-      <Card />
+      {lang === "tr" ? projectsHeader.tr : projectsHeader.en}
+      <div className="projects-main-area">
+        {projectData.map((project, i) => (
+          <div key={i} className="project dark:bg-zinc-800">
+            <div className="project-img">
+              <img src={project.image} />
+            </div>
+            <div className="project-details">
+              <h6 className="dark:text-violet-400">{project.header}</h6>
+              <p className="dark:text-white">
+                {lang === "tr"
+                  ? project.description.tr
+                  : project.description.en}
+              </p>
+              <div className="project-buttons">
+                {project.buttons.map((button, i) => (
+                  <button key={i} className="button dark:bg-violet-400">
+                    {button}
+                  </button>
+                ))}
+              </div>
+              <div className="project-links">
+                {project.links.map((link, i) => (
+                  <a className="dark:text-lime-200" key={i} href="#">
+                    {lang === "tr" ? link.tr : link.en}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
