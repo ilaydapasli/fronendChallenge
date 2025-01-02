00b777e723545b95279b05cc5e5115ac78a801b2
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2025-01-02 16:53:13 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,41 +1,45 @@
-import React from "react";
+import React, { useContext } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faTwitter,
-  faCodepen,
-  faWhatsapp,
-  faInstagram,
-} from "@fortawesome/free-brands-svg-icons";
-
-function Footer() {
+import { SiteGlobalContext } from "../context/SiteGlobalProvider";
+import { footerData } from "../api/data";
+function Footer({ email }) {
+  const { lang, theme } = useContext(SiteGlobalContext);
   return (
-    <div className="footer-container  max-h-screen flex flex-col text-center mt-20 px-4 mb-20">
+    <div
+      className="max-h-screen flex flex-col text-center"
+      style={{ backgroundColor: theme === "dark" ? "#252128" : "#fff" }}
+    >
       <div className="footer-content flex flex-col p-4 ">
-        <h1 className="p-8 text-[48px]" style={{ color: "#4731D3" }}>
-          Send me a message!
+        <h1
+          className="p-8 text-[48px]"
+          style={{ color: theme === "dark" ? "#8F88FF" : "#4731D3" }}
+        >
+          {lang === "tr" ? footerData.sendme.tr : footerData.sendme.en}
         </h1>
-        <p className="p-8 text-[24px] leading-9	">
-          Got a question or proposal, or just want <br /> to say hello? Go
-          ahead.
+        <p
+          className="p-8 text-[24px] leading-9	"
+          style={{ color: theme === "dark" ? "#FFFFFF" : "#120B39" }}
+        >
+          {lang === "tr"
+            ? footerData.description.tr
+            : footerData.description.en}
         </p>
         <a className="text-[#4731D3] leading-9	underline  text-[20px] " href="">
-          almilasucode@gmail.com
+          {email}
         </a>
 
         <div className=" flex gap-2 justify-center mt-3">
-          <FontAwesomeIcon
-            className=""
-            icon={faTwitter}
-            style={{ color: "#4731D3" }}
-          />
-          <FontAwesomeIcon
-            className=""
-            icon={faCodepen}
-            style={{ color: "#4731D3" }}
-          />
-
-          <FontAwesomeIcon icon={faWhatsapp} style={{ color: "#4731D3" }} />
-          <FontAwesomeIcon icon={faInstagram} style={{ color: "#4731D3" }} />
+          {theme === "dark"
+            ? footerData.social_dark.map((e, i) => (
+                <a key={i} href="#">
+                  <img src={e} />
+                </a>
+              ))
+            : footerData.social.map((e, i) => (
+                <a key={i} href="#">
+                  <img src={e} />
+                </a>
+              ))}
         </div>
       </div>
     </div>
