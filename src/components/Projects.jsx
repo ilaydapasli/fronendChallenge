24b5584179e2de78d311ab92b2ad64ddd9203d7e
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2025-01-03 04:39:22 +0300

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,29 +17,29 @@ function Projects() {
       }}
     >
       {/* Sayfa Başlığı */}
-      <h1 className="text-4xl font-bold text-center mb-8 dark:text-white text-violet-600">
+      <h1 className="text-4xl font-bold ml-[360px]   mb-8 text-[#4338CA]">
         {lang === "tr" ? projectsHeader.tr : projectsHeader.en}
       </h1>
 
       {/* Proje Kartları */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div className="flex flex-col items-center gap-8">
         {projectData.map((project, i) => (
           <div
             key={i}
-            className="flex flex-col md:flex-row bg-white dark:bg-zinc-800 shadow-lg rounded-lg overflow-hidden"
+            className="flex flex-col md:flex-row bg-white w-full md:w-[960px] dark:bg-zinc-800 shadow-lg rounded-lg overflow-hidden"
           >
             {/* Proje Resmi */}
             <div className="w-full md:w-1/3">
               <img
                 src={project.image}
                 alt={project.header}
-                className="w-full h-full object-cover"
+                className="w-full h-full object-cover  "
               />
             </div>
 
             {/* Proje Detayları */}
             <div className="w-full md:w-2/3 p-6 flex flex-col justify-between">
-              <h2 className="text-2xl font-semibold mb-4 dark:text-violet-400">
+              <h2 className="text-2xl font-semibold mb-4 text-[#4338CA]">
                 {project.header}
               </h2>
               <p className="text-gray-700 dark:text-gray-300 mb-4">
@@ -51,7 +51,7 @@ function Projects() {
                 {project.buttons.map((button, i) => (
                   <button
                     key={i}
-                    className="bg-violet-500 text-white px-4 py-2 rounded-full text-sm shadow-md hover:bg-violet-600 transition dark:bg-violet-400 dark:hover:bg-violet-500"
+                    className="bg-[#4731D3] text-white px-4 py-2 rounded-full text-sm shadow-md hover:bg-violet-600 transition dark:bg-violet-400 dark:hover:bg-violet-500"
                   >
                     {button}
                   </button>
@@ -62,7 +62,7 @@ function Projects() {
                   <a
                     key={i}
                     href="#"
-                    className="text-violet-500 dark:text-lime-200 hover:underline text-sm"
+                    className="text-[#120B39] hover:underline text-sm"
                   >
                     {lang === "tr" ? link.tr : link.en}
                   </a>
