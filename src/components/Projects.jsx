3b25cfe7c14b32258678658053d24faa13675138
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2025-01-03 12:59:05 +0300

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,7 +17,11 @@ function Projects() {
       }}
     >
       {/* Sayfa Başlığı */}
-      <h1 className="text-4xl font-bold ml-[360px]   mb-8 text-[#4338CA]">
+      <h1
+        className={`text-4xl font-bold ml-[360px] mb-8 ${
+          theme === "light" ? "text-[#4338CA]" : "text-[#C1BAED]"
+        }`}
+      >
         {lang === "tr" ? projectsHeader.tr : projectsHeader.en}
       </h1>
 
@@ -26,7 +30,9 @@ function Projects() {
         {projectData.map((project, i) => (
           <div
             key={i}
-            className="flex flex-col md:flex-row bg-white w-full md:w-[960px] dark:bg-zinc-800 shadow-lg rounded-lg overflow-hidden"
+            className={`flex flex-col md:flex-row bg-white w-full md:w-[960px] shadow-lg rounded-lg overflow-hidden ${
+              theme === "light" ? "bg-white" : "bg-[#2B2727]"
+            }`}
           >
             {/* Proje Resmi */}
             <div className="w-full md:w-1/3">
@@ -39,19 +45,33 @@ function Projects() {
 
             {/* Proje Detayları */}
             <div className="w-full md:w-2/3 p-6 flex flex-col justify-between">
-              <h2 className="text-2xl font-semibold mb-4 text-[#4338CA]">
+              <h2
+                className={`text-2xl font-semibold mb-4 ${
+                  theme === "light" ? "text-[#4338CA]" : "text-[#C1BAED]"
+                }`}
+              >
                 {project.header}
               </h2>
-              <p className="text-gray-700 dark:text-gray-300 mb-4">
+
+              <p
+                className={`mb-4 ${
+                  theme === "light" ? "text-gray-700" : "text-[#FFFFFF]"
+                }`}
+              >
                 {lang === "tr"
                   ? project.description.tr
                   : project.description.en}
               </p>
+
               <div className="flex flex-wrap gap-2 mb-4">
                 {project.buttons.map((button, i) => (
                   <button
                     key={i}
-                    className="bg-[#4731D3] text-white px-4 py-2 rounded-full text-sm shadow-md hover:bg-violet-600 transition dark:bg-violet-400 dark:hover:bg-violet-500"
+                    className={`px-4 py-2 rounded-full text-sm shadow-md transition ${
+                      theme === "light"
+                        ? "bg-[#4731D3] text-white hover:bg-violet-600"
+                        : "bg-[#8173DA] text-white hover:bg-[#6F65C9]"
+                    }`}
                   >
                     {button}
                   </button>
@@ -62,7 +82,9 @@ function Projects() {
                   <a
                     key={i}
                     href="#"
-                    className="text-[#120B39] hover:underline text-sm"
+                    className={`text-sm underline ${
+                      theme === "light" ? "text-[#120B39]" : "text-[#CBF281]"
+                    }`}
                   >
                     {lang === "tr" ? link.tr : link.en}
                   </a>
